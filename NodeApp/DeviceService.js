07064364adedce79eb8c89850f32c3aae3eac0dc
CocoaBob <mengke.wang@gmail.com>
2017-10-19 00:17:56 -0400

    Basic implementation of a service and a characteristic.

---
+++ NodeApp/DeviceService.js
@@ -0,0 +1,17 @@
+var bleno = require('bleno');
+var util = require('util');
+
+var VibrationCharacteristic = require('./characteristics/LoadVibrationCharacterisitc');
+
+function DeviceService() {
+  bleno.PrimaryService.call(this, {
+    uuid: 'fff0',
+    characteristics: [
+      new VibrationCharacteristic()
+    ]
+  });
+};
+
+util.inherits(DeviceService, bleno.PrimaryService);
+
+module.exports = DeviceService;
