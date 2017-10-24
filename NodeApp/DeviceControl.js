7919230dee9ddd09de82ea632a22b5201e131434
CocoaBob <mengke.wang@gmail.com>
2017-10-24 12:08:56 -0400

    Added Python-Shell support.

--- NodeApp/DeviceControl.js
+++ NodeApp/DeviceControl.js
@@ -1,3 +1,5 @@
+var PythonShell = require('python-shell');
+
 var DeviceControl = function() {
 
 };
@@ -8,6 +10,22 @@ DeviceControl.prototype.stopVibration = function () {
 
 DeviceControl.prototype.viberateContinuously = function (intensity) {
 	console.log('- ViberateContinuously:\n  intensity =' + intensity);
+
+  // https://github.com/extrabacon/python-shell
+
+  // var options = {
+  //   mode: 'text',
+  //   pythonPath: 'path/to/python',
+  //   pythonOptions: ['-u'],
+  //   scriptPath: 'path/to/my/scripts',
+  //   args: ['value1', 'value2', 'value3']
+  // };
+
+  // PythonShell.run('my_script.py', options, function (err, results) {
+  //   if (err) throw err;
+  // // results is an array consisting of messages collected during execution
+  //   console.log('results: %j', results);
+  // };
 };
 
 DeviceControl.prototype.viberateInCycle = function (duration, startIntensity, middleIntensity, endIntensity, segmentRatioToMiddle, segmentRatioToEnd, shouldRepeat) {
